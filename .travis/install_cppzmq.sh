bb6509955a6c35d0723a94200a61546a86fa8423
Grzegorz Kowalski <g.kowalski@uj.edu.pl>
2019-02-14 11:41:41 +0100

    workaroud for cppzmq on CentOS7

--- .travis/install_cppzmq.sh
+++ .travis/install_cppzmq.sh
@@ -2,6 +2,12 @@
 
 docker exec cpp_tango mkdir -p /home/tango/cppzmq/build
 
+if [ "$OS_TYPE" = "centos7" ]
+then
+	# workaroud for CentOS 7 not providing static lib for libzmq
+	docker exec cpp_tango sed -e "s/ZeroMQ_LIBRARY AND ZeroMQ_STATIC_LIBRARY/ZeroMQ_LIBRARY OR ZeroMQ_STATIC_LIBRARY/" /home/tango/cppzmq/libzmqPkgConfigFallback.cmake > /home/tango/cppzmq/libzmqPkgConfigFallback.cmake
+fi
+
 echo "Build cppzmq"
 docker exec cpp_tango cmake -H/home/tango/cppzmq -B/home/tango/cppzmq/build -DCMAKE_INSTALL_PREFIX=/home/tango
 if [ $? -ne "0" ]
